9f4116b1649f2f0e3340b147fd2be4fd8fd1bf1e
Vitor Barbosa <Vitorbarbosa062@gmail.com>
2024-10-18 22:40:11 -0300

    Início do projeto

---
+++ ClimaTempo/ViewModels/PrevisaoViewModel.cs
@@ -0,0 +1,32 @@
+﻿using ClimaTempo.Models;
+using ClimaTempo.Services;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ClimaTempo.ViewModels
+{
+    public partial class PrevisaoViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string cidade;
+        private Previsao previsao;
+
+        public ICommand BuscarPrevisaoCommand{ get; }
+
+        public PrevisaoViewModel()
+        {
+            BuscarPrevisaoCommand = new Command(BuscarPrevisao);
+        }
+
+        public async void BuscarPrevisao()
+        {
+            previsao = await new PrevisaoService().GetPrevisaoById(244);
+            Cidade = previsao.Cidade;
+        }
+    }
+}
